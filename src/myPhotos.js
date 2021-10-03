7b918cc0801f531774431260a93742a8953f3c43
Corbin McNeill <corbin.mc96@gmail.com>
2021-10-02 23:39:42 -0700

    reorder photos

--- src/myPhotos.js
+++ src/myPhotos.js
@@ -1,36 +1,6 @@
 export const myPhotos = [
   {
-    src: "https://website-photos-989832117645.s3.amazonaws.com/DSC00278.jpg",
-    width: 1,
-    height: 1
-  },
-  {
-    src: "https://website-photos-989832117645.s3.amazonaws.com/DSC00716.jpg",
-    width: 1,
-    height: 1
-  },
-  {
-    src: "https://website-photos-989832117645.s3.amazonaws.com/DSC00758.jpg",
-    width: 3,
-    height: 2
-  },
-  {
-    src: "https://website-photos-989832117645.s3.amazonaws.com/DSC00916.jpg",
-    width: 3,
-    height: 2
-  },
-  {
-    src: "https://website-photos-989832117645.s3.amazonaws.com/DSC01182.jpg",
-    width: 3,
-    height: 2
-  },
-  {
-    src: "https://website-photos-989832117645.s3.amazonaws.com/DSC01230.jpg",
-    width: 1,
-    height: 1
-  },
-  {
-    src: "https://website-photos-989832117645.s3.amazonaws.com/DSC02078.jpg",
+    src: "https://website-photos-989832117645.s3.amazonaws.com/DSC02445-HDR-Enhanced.jpg",
     width: 3,
     height: 2
   },
@@ -40,8 +10,38 @@ export const myPhotos = [
     height: 3
   },
   {
-    src: "https://website-photos-989832117645.s3.amazonaws.com/DSC02445-HDR-Enhanced.jpg",
+    src: "https://website-photos-989832117645.s3.amazonaws.com/DSC02078.jpg",
     width: 3,
     height: 2
+  },
+  {
+    src: "https://website-photos-989832117645.s3.amazonaws.com/DSC01230.jpg",
+    width: 1,
+    height: 1
+  },
+  {
+    src: "https://website-photos-989832117645.s3.amazonaws.com/DSC01182.jpg",
+    width: 3,
+    height: 2
+  },
+  {
+    src: "https://website-photos-989832117645.s3.amazonaws.com/DSC00916.jpg",
+    width: 3,
+    height: 2
+  },
+  {
+    src: "https://website-photos-989832117645.s3.amazonaws.com/DSC00758.jpg",
+    width: 3,
+    height: 2
+  },
+  {
+    src: "https://website-photos-989832117645.s3.amazonaws.com/DSC00716.jpg",
+    width: 1,
+    height: 1
+  },
+  {
+    src: "https://website-photos-989832117645.s3.amazonaws.com/DSC00278.jpg",
+    width: 1,
+    height: 1
   }
 ];
